c19bb964f001f994b9029aebc168253d48a12e9a
isabelle may salmon <845868806@bun2-206-13.local>
2018-11-21 12:37:53 +1300

    started homepage

--- header.php
+++ header.php
@@ -2,12 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
     <title>izsi salmon</title>
   </head>
   <body>
 
-    <div class="nav">
-
+    <div class="nav caps">
+      <a href="projects.php">projects</a>
+      <a href="contact.php">contact</a>
     </div>
--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php require 'header.php'; ?>
 
-    <div class="main">
+    <div class="main-splash">
 
+      <div class="splash-content">
+
+        <h1>izsi salmon</h1>
+        <div class="splash-location text-secondary caps">Wellington, NZ</div>
+
+        <div class="splash-nav caps">
+          <div class="splash-nav-flex">
+            <a href="webdev.php">Web Dev</a>
+            <i class="fas fa-circle"></i>
+            <a href="graphic.php">Graphic Design</a>
+            <i class="fas fa-circle"></i>
+            <a href="photog.php">Photography</a>
+          </div>
+        </div>
+
+      </div>
     </div>
 
 <?php require 'footer.php'; ?>
