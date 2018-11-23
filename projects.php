43c718caece51b0f80896082d4edf49ff4df0122
isabelle may salmon <845868806@bun2-206-08.local>
2018-11-23 15:40:35 +1300

    added contacts page

--- projects.php
+++ projects.php
@@ -1,4 +1,25 @@
-<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>izsi salmon</title>
+  </head>
+  <body>
+
+    <div class="background-colour">
+
+      <div class="link-home">
+        <a href="index.php">izsi</a>
+      </div>
+
+      <div class="nav caps">
+        <a href="projects.php">projects</a>
+        <a href="contact.php">contact</a>
+      </div>
+
 
 <div class="main-centred">
 
@@ -18,5 +39,5 @@
 
   </div>
 </div>
-
+</div>
 <?php require 'footer.php'; ?>
