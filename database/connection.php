3be5c2642cd2f2b9bc74117e20dfcdd361b0c417
isabelle may salmon <845868806@bun2-206-13.local>
2018-11-25 21:14:44 +1300

    broke everything by trying to add a database :)

---
+++ database/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+  // database connection
+  // $dbc = mysqli_connect(host, username, password, table);
+  $dbc = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_TABLE'));
+  if($dbc){
+    var_dump('connection successful');
+    $dbc->set_charset('utf8');
+  } else{
+    die('Connect to database failed, please check your environment files and make sure username, password and table names are coorect.');
+  }
+
+ ?>
--- header.php
+++ header.php
@@ -1,3 +1,20 @@
+<?php
+
+  if(is_dir('vendor')){
+    require 'vendor/autoload.php';
+  } else{
+    require '../vendor/autoload.php';
+  }
+
+  // Getting the project URL from the environment file – dynamic alternative to hard coding the URL
+  $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+  $dotenv->load();
+  $baseURL = getenv('PROJECT_URL');
+
+  require('database/connection.php');
+
+  ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,9 +22,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
-    <title>ADMIN</title>
+    <title>izsi salmon</title>
   </head>
-  <body>
-    <h1>yo :)</h1>
-  </body>
-</html>
